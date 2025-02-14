f05d5de8ebfb15bdb4632020445a786145869191
chiocomachi <chiocomachi@gmail.com>
2025-02-14 19:25:40 +0900

    slugの修正

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -1,21 +1,19 @@
-import { notFound } from 'next/navigation';
-import { getNewsDetail } from '@/app/_libs/microcms';
-import Article from '@/app/_components/Article';
-import ButtonLink from '@/app/_components/ButtonLink';
-import styles from './page.module.css';
+import { notFound } from "next/navigation";
+import { getNewsDetail } from "@/app/_libs/microcms";
+import Article from "@/app/_components/Article";
+import ButtonLink from "@/app/_components/ButtonLink";
+import styles from "./page.module.css";
 
 type Props = {
-  params: {
-    slug: string;
-  };
-  searchParams: {
-    dk?: string;
-  };
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ dk?: string }>;
 };
 
 export default async function Page({ params, searchParams }: Props) {
-  const data = await getNewsDetail(params.slug, {
-    draftKey: searchParams.dk,
+  const { slug } = await params;
+  const { dk } = await searchParams;
+  const data = await getNewsDetail(slug, {
+    draftKey: dk,
   }).catch(notFound);
 
   return (
